3dd27ec02711ef05d37f800afb80a5573b7b3ec5
I306565 <s.kc@sap.com>
2014-09-24 13:34:07 +0530

    initial state

--- src/main/java/com/ariba/demo/App.java
+++ src/main/java/com/ariba/demo/App.java
@@ -10,20 +10,8 @@ public class App
        public static String now()
        {
 
-
-           Calendar now = Calendar.getInstance();
-               int hour = now.get(Calendar.HOUR_OF_DAY);
-             
-               String greeting = ". Now it is time to say ";
-            
-                             if (hour < 12)
-                            greeting += " Good morning.";
-                             else if (hour < 17)
-                            greeting += "Good afternoon.";
-                             else if (hour < 19)
-                            greeting += "Good evening.";
-                             else greeting += "Good night.";
-                     return (new java.util.Date().toString()) + greeting ; 
-
+    	   // using this you can get the time string.
+           return new java.util.Date().toString(); 
+    
        }
 }
